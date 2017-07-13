f9873ab7caed56e702bf772c7050238399617483
Leo Antunes <leo@costela.net>
2017-07-14 01:06:45 +0200

    tests: add check for staging url

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,6 @@ from click.testing import CliRunner
 from testfixtures import LogCapture
 from acme import client
 
-from wile.lazyclient import LazyClient
-
 if sys.version_info.major < 3:
     import backports.tempfile as tempfile
 else:
@@ -43,12 +41,3 @@ def acmeclientmock(monkeypatch):
     acmeClientMock = Mock(spec_set=[])
     monkeypatch.setattr(client, 'Client', acmeClientMock)
     return acmeClientMock
-
-
-@pytest.fixture()
-def objmock():
-    '''
-    This mocks the click "obj" argument passed around between different subcommands
-    '''
-    objMock = Mock(spec_set=LazyClient)
-    return objMock
--- tests/test_reg.py
+++ tests/test_reg.py
@@ -21,6 +21,21 @@ def test_wile_register__no_args(clirunner, logcapture, acmeclientmock):
     acmeclientmock.assert_called_once_with(wile.LETSENCRYPT_URL, ANY)
 
 
+def test_wile_register__staging(clirunner, logcapture, acmeclientmock):
+    result = clirunner.invoke(wile.wile, args=['--staging', 'register'], obj=LazyClient())
+    # should fail without --auto-accept-tos, but after creating a key
+    assert result.exit_code == 2
+    assert 'Accept Terms of Service?' in result.output
+    logcapture.check(
+        ('wile', 'WARNING',
+         'no account key found; creating a new 2048 bit key in %s' % os.path.expanduser('~/.wile/account.key'))
+    )
+    assert os.listdir(os.path.expanduser('~')) == ['.wile']
+    assert os.listdir(os.path.expanduser('~/.wile')) == ['account.key']
+    assert os.listdir(os.curdir) == []
+    acmeclientmock.assert_called_once_with(wile.LETSENCRYPT_STAGING_URL, ANY)
+
+
 def test_wile_register__help(clirunner, logcapture, acmeclientmock):
     result = clirunner.invoke(wile.wile, args=['register', '--help'], obj=LazyClient())
     assert result.exit_code == 0
@@ -34,6 +49,7 @@ def test_wile_register__autoaccept(clirunner, logcapture, acmeclientmock):
     assert os.listdir(os.path.expanduser('~')) == []
     result = clirunner.invoke(wile.wile, args=['register', '--auto-accept-tos'], obj=LazyClient())
     assert result.exit_code == 0
+    assert 'Accept Terms of Service?' not in result.output
     logcapture.check(
         ('wile', 'WARNING',
          'no account key found; creating a new 2048 bit key in %s' % os.path.expanduser('~/.wile/account.key'))
